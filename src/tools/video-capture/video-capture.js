1684c77f29b8c35a92a4c4be8e6f0757c88a7a6f
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2020-03-31 00:10:15 +0200

    wip css loader

--- src/tools/video-capture/video-capture.js
+++ src/tools/video-capture/video-capture.js
@@ -1,5 +1,7 @@
 import qs from "qs";
 import * as cocoSsd from "@tensorflow-models/coco-ssd/dist/coco-ssd";
+import './video-capture.css';
+
 const getCallbackName = () => {
   let scripts = Array.from(document.getElementsByTagName('script'));
   let myScriptProd = scripts.filter(script => script.src.includes(`dist/tools/video-capture/video-capture.min.js`))[0];
@@ -21,6 +23,54 @@ const detect = async (imageElement) => {
   console.log('detect - prediction: ', predictions);
 }
 
+const detectFromVideo = async (videoElement, canvasElement) => {
+  console.log('detectFromVideo', {cocoSsd, window});
+  const renderPredictions = (predictions, canvasElement) => {
+    const ctx = canvasElement.getContext("2d");
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // Font options.
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      const width = prediction.bbox[2];
+      const height = prediction.bbox[3];
+      // Draw the bounding box.
+      ctx.strokeStyle = "#00FFFF";
+      ctx.lineWidth = 4;
+      ctx.strokeRect(x, y, width, height);
+      // Draw the label background.
+      ctx.fillStyle = "#00FFFF";
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10); // base 10
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    });
+
+    predictions.forEach(prediction => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      // Draw the text last to ensure it's on top.
+      ctx.fillStyle = "#000000";
+      ctx.fillText(prediction.class, x, y);
+    });
+  };
+  // Load the model.
+  const model = await cocoSsd.load();
+  // Classify the image.
+  const detectFrame = async () => {
+    await model.detect(videoElement).then(predictions => {
+      console.log('detectFromVideo - prediction: ', predictions);
+      renderPredictions(predictions, canvasElement);
+      requestAnimationFrame(async () => {
+         await detectFrame(videoElement, model);
+      });
+    });
+  }
+  await detectFrame(videoElement, canvasElement);
+}
+
 const runCallback = () => {
   console.log(`runCallback`);
   window.onload = function(){
@@ -28,7 +78,8 @@ const runCallback = () => {
     let paths = callbackName.split(".");
     window[`${paths[0]}`] = {
       ...window[`${paths[0]}`],
-      detect
+      detect,
+      detectFromVideo
     }
     window[`${paths[0]}`][`${paths[1]}`]();
   }
--- webpack.config.debug.js
+++ webpack.config.debug.js
@@ -1,16 +1,57 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 module.exports = {
-  entry: './src/tools/video-capture/video-capture.js',
+  entry: {
+    "video-capture": './src/tools/video-capture/video-capture.js'
+  },
   output: {
     library: 'video-capture',
     libraryTarget: 'umd',
     auxiliaryComment: 'Video capture library comment',
-    filename: 'video-capture.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'public/dist/tools/video-capture'),
   },
   optimization: {
     // We no not want to minimize our code.
     minimize: false
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      moduleFilename: ({ name }) => `${name}.css`,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        // For pure CSS (without CSS modules)
+        test: /\.css$/i,
+        exclude: /\.module\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        // For CSS modules
+        test: /\.module\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+        },
+      },
+    ],
+  },
 };
